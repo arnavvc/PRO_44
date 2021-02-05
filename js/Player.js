795bb4a6483ac628f03e9a865b0b7f465670c683
arnavvc <72585981+arnavvc@users.noreply.github.com>
2021-02-04 19:05:58 -0800

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,50 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.x = 0;
+    this.y = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getRank(){
+    database.ref('Rank').on("value", (data)=>{
+      this.rank=data.val()
+    })
+  }
+  static updateRank(rank)
+  {
+    database.ref('/').update({Rank:rank})
+    
+  }
+
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      x:this.x,
+      y:this.y
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
